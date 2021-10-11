7a4dbbc511b94e1dd7f3ad0d35443e0311e59fe6
Lenwit <mack.diaz@alumnos.ucn.cl>
2021-10-11 17:47:29 -0300

    CommitValidarRut
    Valida un rut ingresado sin puntos ni guión.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use App\Rules\ValidarRut;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -51,9 +52,9 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'rut' => ['required', 'string', 'max:255', 'unique:users','regex:/^[0-9]+$/'],
+            'rut' => ['required', 'string', 'max:255', 'unique:users', new ValidarRut],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:6', 'confirmed'],
         ]);
     }
 
