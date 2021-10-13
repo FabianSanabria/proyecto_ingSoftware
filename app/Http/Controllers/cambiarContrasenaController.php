2cc4c7488a8094ce94fe75046ac7bbf612cbb210
Fabian0908 <fabian.sanabria@alumnos.ucn.cl>
2021-10-13 13:56:59 -0300

    cambio de contrasenñña listo + intento fallido de uso de sweet alert

---
+++ app/Http/Controllers/cambiarContrasenaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Rules\validarPassword;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use UxWeb\SweetAlert\SweetAlert;
+use App\Providers\SweetAlertServiceProvider;
+
+class cambiarContrasenaController extends Controller
+
+{
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
+    public function __construct()
+
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+
+    public function index()
+
+    {
+        return view('cambiarContrasena');
+    }
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function store(Request $request)
+
+    {
+
+        $request->validate([
+
+            'password' => ['required', new validarPassword],
+
+            'new_password' => ['required','string','min:6'],
+
+            'password_confirmation' => ['same:new_password'],
+
+        ]);
+        User::find(auth()->user()->id)->update(['password'=> Hash::make($request->new_password)]);
+
+       // alert()->success('Contraseña actualizada con éxito')->persistent('Cerrar');
+        SweetAlert::success('Contraseña actualizada con éxito')->persistent('Cerrar');
+        return redirect()->route('home')->with('msg',"password actualizada");
+
+
+    }
+
+}
--- resources/views/cambiarContrasena.blade.php
+++ resources/views/cambiarContrasena.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+@include('sweet::alert')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -8,67 +9,12 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="{{ route('change.password') }}">
                         @csrf
+                        <script src="{{ asset('js/app.js') }}"></script>
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="rut" class="col-md-4 col-form-label text-md-right">{{ __('Rut usuario') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="rut" type="text" class="form-control @error('rut') is-invalid @enderror" name="rut" value="{{ old('rut') }}" required autocomplete="rut">
-
-                                @error('rut')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="status" class="col-md-4 col-form-label text-md-right">{{ __('Estado usuario') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="status" type="checkbox" class="form-control @error('status') is-invalid @enderror" name="status" >
-
-                                @error('status')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña antigua') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -82,29 +28,29 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña nueva') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="new_password" type="password" class="form-control @error('new_password') is-invalid @enderror" name="new_password" required autocomplete="new-password">
+
+                                @error('new_password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar nueva contraseña') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
-                        <div class="form-group row">
 
-                            <div class="col-md-6">
-
-                                <select name="rolSelect" id= "rolSelect"aria-describedby="validationServer04Feedback" required>
-                                    <option selected disabled value="Rol">Rol</option>
-                                    <option value="Estudiante">Estudiante</option>
-                                    <option value="Jefe de Carrera">Jefe de Carrera</option>
-                                </select>
-                                <div id="validationServer04Feedback" class="invalid-feedback">
-                                    @error('rolSelect')
-                                        <strong>{{ $message }}</strong>
-                                    @enderror
-                                </div>
-                            </div>
-                        </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
