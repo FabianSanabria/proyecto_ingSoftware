5d2e03f5c5d32dd77712464b2a9fe4621cb97c18
Fabian0908 <fabian.sanabria@alumnos.ucn.cl>
2021-10-15 13:36:50 -0300

    Pagina administrar usuario implementada

--- resources/views/usuario/index.blade.php
+++ resources/views/usuario/index.blade.php
@@ -3,24 +3,54 @@
 @section('content')
 
 <div class="container">
-    <div class="row mb-3">
+    <div class="row mb-4">
 
-        <div class="col col-8">
-            <p class="text-center" style="font-size: x-large">Administrar Usuarios</p>
-        </div>
-        <div class="card-footer">
-            <a href="/crearUsuario" class="btn btn-info btn-block">Crear Usuario</a>
+        <div class="col">
+            <p class="text-center" style="font-size: x-large; text-align: center;">Administrar Usuarios</p>
         </div>
     </div>
-    <table class="table table">
-        <thead>
-            <tr>
-                <th style="width: 10%" scope="col">Código</th>
-                <th style="width: 70%" scope="col">Nombre</th>
-                <th style="width: 20%" scope="col" colspan="1">Accion</th>
-            </tr>
-
-    </table>
+    <div class ="card-deck">
+        <div class="card">
+            <i class="fas fa-users fa-10x text-center" style ="color:dodgerblue"></i>
+            <div class="card-body">
+                <h5 class="card-title text-center">Crear</h5>
+                <small class="text-muted">Permite crear estudiantes y jefes de carrera.</small>
+            </div>
+            <div class="card-footer">
+                <a href="/crearUsuario" class="btn btn-info btn-block">IR</a>
+            </div>
+        </div>
+        <div class="card">
+            <i class="fas fa-graduation-cap fa-10x text-center"style ="color:dodgerblue"></i>
+            <div class="card-body">
+                <h5 class="card-title text-center">Editar Datos</h5>
+                <small class="text-muted">Permite modificar datos de los usuarios.</small>
+            </div>
+            <div class="card-footer">
+                <a href="/carrera" class="btn btn-info btn-block">IR</a>
+            </div>
+        </div>
+        <div class="card">
+            <i class="fas fa-graduation-cap fa-10x text-center"style ="color:dodgerblue"></i>
+            <div class="card-body">
+                <h5 class="card-title text-center">Habilitar / Desabilitar</h5>
+                <small class="text-muted">.</small>
+            </div>
+            <div class="card-footer">
+                <a href="/carrera" class="btn btn-info btn-block">IR</a>
+            </div>
+        </div>
+        <div class="card">
+            <i class="fas fa-graduation-cap fa-10x text-center"style ="color:dodgerblue"></i>
+            <div class="card-body">
+                <h5 class="card-title text-center">Reiniciar Clave</h5>
+                <small class="text-muted">Permite reiniciar clave de usuario a clave.</small>
+            </div>
+            <div class="card-footer">
+                <a href="/carrera" class="btn btn-info btn-block">IR</a>
+            </div>
+        </div>
+    </div>
 </div>
 
 
