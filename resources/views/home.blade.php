e099300f1b89127dfd4f6194d06766a9883ee5d3
AllanCortes <91397441+AllanCortes@users.noreply.github.com>
2021-10-18 04:19:46 -0300

    de nuevo iconos
    iconos + color

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         @if (Auth::user()->rol == 2)
         <div class ="card-deck">
             <div class="card">
-                <i class="fas fa-users fa-10x text-center" style="background-color: #003057;"></i>
+                <i class="fas fa-users fa-10x text-center" style="color: #003057;"></i>
                 <div class="card-body">
                     <h5 class="card-title text-center">Administrar usuarios</h5>
                     <small class="text-muted">Permite o crear/editar/deshabilitar usuarios del sistema.</small>
@@ -16,7 +16,7 @@
                 </div>
             </div>
             <div class="card">
-                <i class="fas fa-graduation-cap fa-10x text-center"style ="color:dodgerblue"></i>
+                <i class="fas fa-graduation-cap fa-10x text-center"style="color: #003057;"></i>
                 <div class="card-body">
                     <h5 class="card-title text-center">Administrar Carreras</h5>
                     <small class="text-muted">Permite crear y/o editar carreras en el sistema.</small>
